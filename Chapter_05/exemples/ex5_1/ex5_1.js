0eeaa29761992f60b705bc6d7f2bb70b0a352721
Rafael Costa <rcostadesignweb@gmail.com>
2022-07-19 17:15:03 -0300

    Chapter 5 - Example 01

--- Chapter_05/exemples/ex5_1/ex5_1.js
+++ Chapter_05/exemples/ex5_1/ex5_1.js
@@ -1,26 +1,67 @@
 
+const inPaciente = document.getElementById('inPaciente');
+const paciente = inPaciente.value;
+
+const outLista = document.getElementById('outLista');
+const outAtendimento = document.getElementById('outAtendimento');
+
 const btnAdicionar = document.getElementById('btnAdicionar');
+const btnUrgencia = document.getElementById('btnUrgencia');
+const btnAtender = document.getElementById('btnAtender');
 
 let pacientes = [];
 
-function adicionarCliente() {
-  const inPaciente = document.getElementById('inPaciente');
-  const paciente = inPaciente.value;
-  const outLista = document.getElementById('outLista');
+function atenderPaciente() {
+  if(pacientes.length === 0) {
+    alert('Fila de atendimento está vazia!');
+    inPaciente.focus();
+    return
+  }
 
-  if(paciente === '') {
+  outAtendimento.innerHTML = pacientes[0];
+  pacientes.shift();
+
+  for (let index = 0; index < pacientes.length; index++) {
+    outLista.innerHTML = pacientes.join('<br>');
+  }
+
+}
+
+function urgenciaPaciente() {
+  if(inPaciente.value === '') {
     alert('Digite o nome do paciente');
     inPaciente.focus();
     return
   }
 
-  pacientes.push(paciente);
+  pacientes.unshift(inPaciente.value);
 
   for (let index = 0; index < pacientes.length; index++) {
     outLista.innerHTML = pacientes.join('<br>');
   }
 
   inPaciente.value = ''
+  inPaciente.focus()
 }
 
-btnAdicionar.addEventListener('click', adicionarCliente);
+function adicionarPaciente() {
+
+  if(inPaciente.value === '') {
+    alert('Digite o nome do paciente');
+    inPaciente.focus();
+    return
+  }
+
+  pacientes.push(inPaciente.value);
+
+  for (let index = 0; index < pacientes.length; index++) {
+    outLista.innerHTML = pacientes.join('<br>');
+  }
+
+  inPaciente.value = ''
+  inPaciente.focus()
+}
+
+btnAdicionar.addEventListener('click', adicionarPaciente);
+btnUrgencia.addEventListener('click', urgenciaPaciente);
+btnAtender.addEventListener('click', atenderPaciente);
